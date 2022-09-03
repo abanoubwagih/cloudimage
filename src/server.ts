aa7bbd2e254da396462b102e7b543dbc37dc2b08
Abanoub Wagih <abanoub.wagih@allegiancemd.com>
2022-09-03 23:57:18 +0200

    first commit

---
+++ src/server.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  app.get("/filteredimage/", async (req: Request, res: Response) => {
+    const image_url = req.query.image_url.toString();
+
+    if (!image_url) {
+        res.status(400).send("image url is required");
+    }
+    const filtered_image = await filterImageFromURL(image_url);
+    res.status(200).sendFile(filtered_image, () => {
+        deleteLocalFiles([filtered_image]);
+    });
+});
+
+
+//! END @TODO1
+
+// Root Endpoint
+// Displays a simple message to the user
+app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+});
+
+
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
